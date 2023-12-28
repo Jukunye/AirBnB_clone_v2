772217a662d8a88dfb877142d0fb60688c81e95f
Jukunye <jukunyes@gmail.com>
2023-12-28 22:05:53 +0300

    Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Route handler for the root path ("/").
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """
+    Starts the Flask app on 0.0.0.0 and port 5000.
+    """
+    app.run(host='0.0.0.0', port=5000)
