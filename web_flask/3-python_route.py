2515c00e693a26fd8e32cbf6518c477774e1d1ef
Jukunye <jukunyes@gmail.com>
2023-12-28 23:27:40 +0300

    route python default

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,9 +31,9 @@ def c_fun(text):
     return "C {}".format(text).replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_func(text):
+def python_func(text="is cool"):
     """
     Route handler for the root path ("/python").
     """
