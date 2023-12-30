7bb311d407837ff2ccef2e58b015450201a170df
Jukunye <jukunyes@gmail.com>
2023-12-30 07:10:03 +0300

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+ script that starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Route handler for the root path ("/").
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the root path ("/hbnb").
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    """
+    Route handler for the root path ("/c").
+    """
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_func(text="is cool"):
+    """
+    Route handler for the root path ("/python").
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    """
+    Starts the Flask app on 0.0.0.0 and port 5000.
+    """
+    app.run(host='0.0.0.0', port=5000)
