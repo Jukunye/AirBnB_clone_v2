b7f6307b21a331cdfc6293ae3910e3c64b7de695
Jukunye <jukunyes@gmail.com>
2023-12-30 07:31:54 +0300

    add number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+ script that starts a Flask web application:
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Route handler for the root path ("/").
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the root path ("/hbnb").
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    """
+    Route handler for the root path ("/c").
+    """
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_func(text="is cool"):
+    """
+    Route handler for the root path ("/python").
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Route handler for the root path ("/number/").
+    """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_tamplate(n):
+    """
+    Route handler for the root path ("/number_template/").
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    """
+    Starts the Flask app on 0.0.0.0 and port 5000.
+    """
+    app.run(host='0.0.0.0', port=5000)
