e538ca24096780bf9c27c445f02413d33035c773
Jukunye <jukunyes@gmail.com>
2023-12-30 10:20:46 +0300

    db_storage doc string

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Module to provide a database storage interface using SQLAlchemy
+"""
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,10 +17,17 @@ import sys
 
 
 class DBStorage():
+    """
+    Database storage class using SQLAlchemy
+    for interaction with a MySQL database.
+    """
     __engine = None
     __session = None
 
     def __init__(self):
+        """
+        Initialize the DBStorage instance and create the database engine.
+        """
         env = getenv('HBNB_ENV')
         user = getenv('HBNB_MYSQL_USER')
         password = getenv('HBNB_MYSQL_PWD')
@@ -31,6 +42,16 @@ class DBStorage():
             pass
 
     def all(self, cls=None):
+        """
+        Query and return all objects from the database.
+
+        Args:
+            cls (str): The name of the class to query.
+                        If None, query all objects from all tables.
+
+        Returns:
+            dict: A dictionary of objects, format {'classname.id': object}.
+        """
         classes = {'State': State, 'City': City, 'Amenity': Amenity,
                    'Place': Place, 'Review': Review, 'User': User}
         if cls in classes.keys():
@@ -48,16 +69,34 @@ class DBStorage():
         return {f'{obj.__class__.__name__}.{obj.id}': obj for obj in objects}
 
     def new(self, obj):
+        """
+        Add a new object to the current database session.
+
+        Args:
+            obj: The object to add to the session.
+        """
         self.__session.add(obj)
 
     def save(self):
+        """
+        Commit changes to the database session.
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
+        """
+        Delete an object from the database session.
+
+        Args:
+            obj: The object to delete from the session.
+        """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
+        """
+        Create all tables in the database and set up a new session.
+        """
         Base.metadata.create_all(self.__engine)
 
         session_factory = sessionmaker(
@@ -66,4 +105,7 @@ class DBStorage():
         self.__session = Session()
 
     def close(self):
+        """
+        Close the current database session.
+        """
         self.__session.close()
